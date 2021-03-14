029238b83e91684b8a481d77b7d49111afef7b30
ntokala <nikhiltokala1@gmail.com>
2021-03-13 22:23:31 -0500

    Created CameraShift.cs and PlayerController.cs

---
+++ Assets/Scripts/CameraShift.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShift : MonoBehaviour
+{
+    public Camera cam1, cam2, cam3;
+
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam1.gameObject.SetActive(false);
+        cam2.gameObject.SetActive(true);
+        cam3.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (Input.GetKeyDown("c"))
+        {
+            cam1.gameObject.SetActive(true);
+            cam2.gameObject.SetActive(false);
+            cam3.gameObject.SetActive(false);
+        }
+        if (Input.GetKeyDown("v"))
+        {
+            cam1.gameObject.SetActive(false);
+            cam2.gameObject.SetActive(true);
+            cam3.gameObject.SetActive(false);
+            
+        }
+        if (Input.GetKeyDown("b"))
+        {
+            cam1.gameObject.SetActive(false);
+            cam2.gameObject.SetActive(false);
+            cam3.gameObject.SetActive(true);
+        }
+    }
+}
