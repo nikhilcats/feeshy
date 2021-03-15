f2eb704bd5f6cc838343f1dfed1842d2baa10671
hauntedw <hindsedward@gmail.com>
2021-03-15 18:26:13 -0400

    item shop changes
    shop works now still using the array system not on database yet. shop on its own scene now

--- Assets/Scripts/ButtonInfo.cs
+++ Assets/Scripts/ButtonInfo.cs
@@ -5,15 +5,15 @@ using UnityEngine.UI;
 
 public class ButtonInfo : MonoBehaviour
 {
+
     public int ItemID;
-    public Text PriceTxt;
+    public Text PirceTxt;
     public Text QuantityTxt;
     public GameObject ShopManager;
-    
-    // Update is called once per frame
+
     void Update()
     {
-        PriceTxt.text = "Price: $" + ShopManager.GetComponent<ShopManagerScript>().shopItems[2, ItemID].ToString();
+        PirceTxt.text = "Price: $" + ShopManager.GetComponent<ShopManagerScript>().shopItems[2, ItemID].ToString();
         QuantityTxt.text = ShopManager.GetComponent<ShopManagerScript>().shopItems[3, ItemID].ToString();
     }
 }
