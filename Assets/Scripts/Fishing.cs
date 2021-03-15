a5eb38ec0e45375c78280b785c0f03589abc54cb
AlfyyyDev <34554191+AlfyyyDev@users.noreply.github.com>
2021-03-15 13:23:58 -0400

    Updated Fishing scripts to interact with DB
    Created DB interactions in fishing script, attack fishing action to fishing pole in final game and attach DB to that script

--- Assets/Scripts/Fishing.cs
+++ Assets/Scripts/Fishing.cs
@@ -5,37 +5,46 @@ using UnityEngine;
 public class Fishing
 {
     public List<Item> CatchableObjects = new List<Item>();
-    public Item OrangeFish;
-    public Item GreenFish;
-    public Item BlueFish;
-    public Item RainbowFish;
+    public Item orangeFish;
+    public Item greenFish;
+    public Item blueFish;
+    public Item rainbowFish;
 
-    public Item CastLine()
+    public Item CastLine(ItemDatabase itemDatabase)
     {
+        
+        ItemDatabase db = itemDatabase;
         System.Random random = new System.Random();
         Debug.Log("line has been cast");
-        CatchableObjects.Add(GreenFish);
-        CatchableObjects.Add(GreenFish);
-        CatchableObjects.Add(GreenFish);
-        CatchableObjects.Add(GreenFish);
-        CatchableObjects.Add(GreenFish);
-        CatchableObjects.Add(GreenFish);
-        CatchableObjects.Add(GreenFish);
-        CatchableObjects.Add(GreenFish);
-        CatchableObjects.Add(BlueFish);
-        CatchableObjects.Add(BlueFish);
-        CatchableObjects.Add(BlueFish);
-        CatchableObjects.Add(BlueFish);
-        CatchableObjects.Add(BlueFish);
-        CatchableObjects.Add(BlueFish);
-        CatchableObjects.Add(BlueFish);
-        CatchableObjects.Add(BlueFish);
-        CatchableObjects.Add(OrangeFish);
-        CatchableObjects.Add(OrangeFish);
-        CatchableObjects.Add(RainbowFish);
+
+        //setting fish to equivalent db spots
+        blueFish = db.GetItem(1);
+        greenFish = db.GetItem(2);
+        orangeFish = db.GetItem(3);
+        rainbowFish = db.GetItem(4);
+
+        //Populates list with weighted catchable objects, currently out of 100, first for loop populates 80 of most common fish
+        for (int i = 0; i < 40; i++)
+        {
+            CatchableObjects.Add(greenFish);
+            CatchableObjects.Add(blueFish);
+        }
+        for (int i = 0; i < 15; i++)
+        {
+            CatchableObjects.Add(orangeFish);
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            CatchableObjects.Add(rainbowFish);
+        }
+        Debug.Log(CatchableObjects.Count);
         int index = random.Next(CatchableObjects.Count);
         Debug.Log(CatchableObjects[index]);
+
         Item objectCaught = CatchableObjects[index];
+
+        //depopulates list so that it doesn't overflow very important :)
+        CatchableObjects.Clear();
         return objectCaught;
     }
 }
--- Assets/Scripts/FishingAction.cs
+++ Assets/Scripts/FishingAction.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class FishingAction : MonoBehaviour
 {
     Fishing fishing = new Fishing();
+    public ItemDatabase itemDatabase;
+
+
     private void OnMouseDown()
     {
         Debug.Log("Ive been clicked UWU");
-        fishing.CastLine();
+        Item fishCaught = fishing.CastLine(itemDatabase);
+        Debug.Log(fishCaught.name);
     }
     
 }
