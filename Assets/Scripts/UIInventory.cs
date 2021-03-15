2fd5378e3cabf83de0085d547d2455fea8a0ed2a
ntokala <nikhiltokala1@gmail.com>
2021-03-15 14:35:39 -0400

    Created UI classes

---
+++ Assets/Scripts/UIInventory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventory : MonoBehaviour
+{
+    public List<UIItem> uiItems = new List<UIItem>();
+    public GameObject slotPrefab;
+    public GameObject slotPanel;
+    public int numberOfSlots = 8;
+
+    private void Awake()
+    {
+        for (int i = 0; i < numberOfSlots; i++)
+        {
+            //uiItems.Add()
+        }
+    }
+}
