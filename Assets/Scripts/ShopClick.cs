0b360e10ebb41888ead6db89f8017776aeb8faf5
ntokala <nikhiltokala1@gmail.com>
2021-03-16 01:50:01 -0400

    Created shop functionality

---
+++ Assets/Scripts/ShopClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopClick : MonoBehaviour
+{
+    public GameObject panel;
+    public void OnMouseDown()
+    {
+        Debug.Log("Shop Opened");
+        panel.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
