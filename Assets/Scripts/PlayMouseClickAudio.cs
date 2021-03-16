dcdd08b567c0360c49d3b0c794024acba52c5f9e
AlfyyyDev <34554191+AlfyyyDev@users.noreply.github.com>
2021-03-16 04:39:57 -0400

    Start Menu Mouse Click Sound
    Added Mouse Click sound to start button1!

---
+++ Assets/Scripts/PlayMouseClickAudio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMouseClickAudio : MonoBehaviour
+{
+    public AudioSource audiosource;
+    public AudioClip mouseClickClip;
+    public float volume = 1f;
+
+    public void PlayMouseClick()
+    {
+        audiosource.PlayOneShot(mouseClickClip, volume);
+    }
+}
