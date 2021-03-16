dfbdbab314d161142c517a87a62b061eb5bb3aa7
hauntedw <hindsedward@gmail.com>
2021-03-15 21:03:05 -0400

    scene change
    shop now on game scene need to make it a pop up

---
+++ Assets/Scripts/PopUp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUp : MonoBehaviour
+{
+    public Button yourButton;
+    //declare a boolean
+    public bool buttonisclicked;
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        //set to false on start, may not be necessary, I just can't remember if C# will return 1 or 0 for undefined booleans 
+        buttonisclicked = false;
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        //set our bool to true
+        buttonisclicked = true;
+    }
+
+    private void OnGUI()
+    {
+        //check if our button click boolean is true
+        if (buttonisclicked)
+        {
+            //create gui menu, refer to linked documentation to learn how to create gui elements via script, and learn about various styling techniques
+        }
+    }
+}
